547322de003cac40a9ef429529cf3ea58522b4f2
abinbinge <abinbinge@gmail.com>
2017-03-08 17:24:41 +0800

    [author:abina, date:20170308, for:update pathUtils file, add main method test]

--- src/com/abina/basetype/PathUtils.java
+++ src/com/abina/basetype/PathUtils.java
@@ -1,5 +1,7 @@
 package com.abina.basetype;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 路径工具类
  * @author abina
@@ -7,4 +9,21 @@ package com.abina.basetype;
  */
 public class PathUtils {
 
+	public static void main(String[] args) {
+		HttpServletRequest request = null;
+		request.getRealPath("");
+		//项目名称, example:food-security
+		request.getContextPath(); 
+		//WEB项目的全路径,explame:F:\apache-tomcat-7.0.73-food\webapps\food-security
+		request.getSession().getServletContext().getRealPath("/");
+		
+		///projectName/testPage.jsp
+		String requestURI = request.getRequestURI();
+		
+		//example:projectName/jsp/testPage.jsp
+		request.getServletPath();
+		
+		//example:D://ProjectName/WebRoot/WEB-INF/classes/...
+		Class.class.getClass().getResource("/").getPath();
+	}
 }
